31bdb6ea4c76c1a6726b81683a31dacc13bf27ab
tokencolour <tokencolour@gmail.com>
2016-12-20 22:01:50 +0530

    Added round constant and Subword lookup

---
+++ aes/rcon_test.py
@@ -0,0 +1,38 @@
+from myhdl import block, StopSimulation, instance, Signal, intbv, always, delay, now, traceSignals
+from rcon import rcon_lut
+@block
+def test():
+	clk = Signal(0)
+	rcon = Signal(intbv(0)[32:])
+	key_load = Signal(0)
+	rcon_inst = rcon_lut(clk, rcon, key_load)
+
+	@always(delay(10))
+	def clkgen():
+		clk.next = not clk
+	
+	@instance
+	def stimulus():
+		key_load.next = 1;
+		#yield clk.negedge
+		key_load.next = 0;
+		for i in range(10):
+			yield clk.negedge
+		yield delay(20)
+		raise StopSimulation()
+
+	@instance
+	def monitor():
+		print ("rcon_entries")
+		while 1:
+			yield clk.posedge
+			print("	%s	%s" % (now(), rcon))
+	return clkgen, stimulus, rcon_inst, monitor
+
+tb = test()
+tb.config_sim(trace=True)
+traceSignals.timescale = "1ps"
+#tb = traceSignals(test)
+tb.run_sim()
+
+			
---
+++ aes/s_box_test.py
@@ -0,0 +1,34 @@
+from myhdl import block, StopSimulation, instance, Signal, intbv, always, delay, now, traceSignals
+from s_box import subword
+
+@block
+def test():
+	out_p = Signal(intbv(0)[8:])
+	in_p = Signal(intbv(0)[8:])
+	s_inst = subword(out_p, in_p)
+	
+	@instance
+	def stimulus():
+		in_p.next = 0xf4
+		yield delay(10)
+		in_p.next = 0xff
+		yield delay(10)
+		in_p.next = 0x01
+		yield delay(10)
+		yield delay(10)
+		yield delay(10)
+		raise StopSimulation()
+		
+	@instance
+	def monitor():
+		print("s_box look up")
+		while 1:
+			yield delay(10)
+			print("	%s	%s  %s" % (now(), in_p, out_p))
+			
+	return stimulus, monitor, s_inst
+	
+tb = test()
+tb.config_sim(trace=True)
+traceSignals.timescale = "1ps"
+tb.run_sim()
